c45b0544ff12906c4b62c655259ea88b97364974
Your Name <neeraj@Neerajs-MacBook-Air.local>
2024-08-06 01:20:02 +0530

    first-commit

---
+++ frontend/src/components/Articles.jsx
@@ -0,0 +1,41 @@
+import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import { getNews } from "../service/api";
+import Article from "./Article";
+// import data from "../../../backend/constants/data.js"
+
+
+const Articles = () => {
+    
+    const[news, setNews] = useState([]);
+
+    useEffect(() =>{
+        dailyNews();
+    },[]);
+
+    const dailyNews = async() => {
+        try {
+            let response = await getNews();
+            setNews(response.data);
+            return response;
+        } 
+        catch (error) {
+            console.log(error);
+            console.log(error.message);
+            
+        }
+        
+    }
+
+    return(
+        <Box>
+                {
+                    news.map(data => (
+                        <Article data= {data} />
+                    ))
+                }
+        </Box>
+    )
+}
+
+export default Articles;
